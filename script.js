549c6c9aa478d23deeffd720476740459a2307f9
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-08-14 18:07:01 +0200

    inizio esercizi JS

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+
+
+
+
+
+const summerButton= document.querySelector('button[aria-controls=summer-content]')
+
+for () {
+
+    button.addEventListeners ('click', function (event){
+
+        alert('hello')
+
+
+
+    })
+    
+}
+
+
+
+
+
+
+}
