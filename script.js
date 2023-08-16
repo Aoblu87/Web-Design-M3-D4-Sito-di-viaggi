3214f3327351248c50af690d217bd385bd6c0307
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-08-16 22:58:14 +0200

    completato esercizio 2-5. problema con esercizio 1

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
 
-const summerCards = document.querySelector('#welcome-summer >div:last-child')
-// const cards= document.querySelectorAll('#welcome-summer>div:last-child .col-6')
+// const summerCards = document.querySelector('#welcome-summer >div:last-child')
+// // const cards= document.querySelectorAll('#welcome-summer>div:last-child .col-6')
+
+const collapseButton = document.querySelectorAll('.collapsible')
+const sections = document.querySelectorAll('.collapse-section')
+
+const removeButton = document.querySelectorAll('.removeButton')
+const countButton = document.querySelector('#count')
+const cards = document.querySelectorAll('.collapse-section>div')
+
+
+const countResult = document.createElement('div')
+countButton.appendChild(countResult)
+// countResult.className = 'col-6'
 
 window.onload = function () {
 
 
 
 
+    // ----------------Dichiaro costanti dei nodi del DOM
 
-    const summerButton = document.querySelectorAll('.collapsible')
-    const summerCards = document.querySelector('#welcome-summer >div:last-child')
 
-    for (button of summerButton) {
+
+
+
+    // -------------------Collapsible Sections--------ESERCIZIO 1
+
+
+    for (const button of collapseButton) {
 
         button.addEventListener('click', function (event) {
 
-                // summerCards.classList.toggle('hidden')
-                summerCards.computedStyleMap.display='none'
-           
-
-
+            for (const section of sections) {
+                // section.classList.toggle('.hidden')
+                section.style.display = 'none'
+            }
         })
-
     }
+}
 
+//--------------Bottoni per rimuovere cards e per contarle--------
 
+//--------------ESERCIZIO 2 RIMUOVI CARDS
 
+for (const button of removeButton) {
 
+    button.addEventListener('click', function (event) {
 
+        for (const card of cards) {
+            card.remove()
+        }
+
+    })
 
 }
+
+//------------ESERCIZIO 5 CONTA CARDS
+
+
+    countButton.addEventListener('click', function (event) {
+        let countCards=0
+        for (const card of cards){
+            countCards ++
+        }
+        countResult.innerHTML = countCards
+
+    }
+    )
+
